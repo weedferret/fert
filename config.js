7fbfeb3e21e1311b1b628a5af751e0b70414a325
Kiji Terrozas <50565893+weedferret@users.noreply.github.com>
2019-05-20 11:10:59 -0400

    Add config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+exports.config = {
+	// Bot token, DO NOT SHARE
+	token: "",
+
+	//	Whether to log debug info
+	debug: false,
+
+	//	Bot owner tag
+	ownerName: "obscvre#7124",
+
+	//Bot owner ID
+	ownerId: "478636591665905684",
+
+	//	Command prefix
+	prefix: ".",
+
+	//	Username bot will use when it starts up
+	botUsername: "Fert!",
+
+	//	The initial "game" the bot will be playing when started
+	initialGame: "hewwo | .help"
+};
